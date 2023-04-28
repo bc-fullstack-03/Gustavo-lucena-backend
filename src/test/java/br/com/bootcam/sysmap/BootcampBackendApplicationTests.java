dc3c86b0ef493eb5a0d0a05ad7b5399127608838
Gustavo Lucena <gustavolucena12@gmail.com>
2023-04-28 08:28:22 -0300

    Iniciando projeto

---
+++ src/test/java/br/com/bootcam/sysmap/BootcampBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bootcam.sysmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
