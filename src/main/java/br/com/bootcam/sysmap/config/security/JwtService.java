c09baef5070c1962d24501d69c9f445018b8aaae
Gustavo Lucena <gustavolucena12@gmail.com>
2023-05-05 17:59:41 -0300

    realocando secret key

--- src/main/java/br/com/bootcam/sysmap/config/security/JwtService.java
+++ src/main/java/br/com/bootcam/sysmap/config/security/JwtService.java
@@ -18,9 +18,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-
-    @Value("jwt.secret.key")
-    private String SECRET_KEY;
+    
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
